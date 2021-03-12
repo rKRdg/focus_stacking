255da6b8b9167df6ecaa450c9f69e1965232e6c1
Rainsoptics <eric@rainsoptics.com>
2021-03-12 09:14:59 +0800

    multiple images stacking

--- focus_stacking.py
+++ focus_stacking.py
@@ -6,7 +6,7 @@ import config
 
 
 
-def align_image(image1,image2):
+def align_image(baseimage,images):
 	print("alignment starts...")
 	if config.ALIGNMENT_DETECTOR=="SIFT":
 		print(f"use SIFT detector with {config.ALIGNMENT_FEATURE_NUM} features")
@@ -15,33 +15,41 @@ def align_image(image1,image2):
 		print(f"use SURF detector with {config.ALIGNMENT_FEATURE_NUM} features")
 		detector = cv2.xfeatures2d.SURF_create(config.ALIGNMENT_FEATURE_NUM)
 
-	kp1, des1 = detector.detectAndCompute(image1, None)
-	kp2, des2 = detector.detectAndCompute(image2, None)
-
-	FLANN_INDEX_KDTREE = 0
-	index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-	search_params = dict(checks=50)
-	flann = cv2.FlannBasedMatcher(index_params, search_params)
-	matches = flann.knnMatch(des1, des2, k=2)
+	kp1, des1 = detector.detectAndCompute(baseimage, None)
 
 
-	good = []
-	for m, n in matches:
-		if m.distance < config.ALIGNMENT_DISTANCE * n.distance:
-			good.append(m)
+
+	for imgi in range(len(images)):
+		image2=images[imgi]
+
+		kp2, des2 = detector.detectAndCompute(image2, None)
+
+		FLANN_INDEX_KDTREE = 0
+		index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+		search_params = dict(checks=50)
+		flann = cv2.FlannBasedMatcher(index_params, search_params)
+		matches = flann.knnMatch(des1, des2, k=2)
 
 
-	src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-	dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+		good = []
+		for m, n in matches:
+			if m.distance < config.ALIGNMENT_DISTANCE * n.distance:
+				good.append(m)
 
-	M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-	
 
-	imgout=cv2.warpPerspective(image2, np.linalg.inv(M), (image2.shape[1], image2.shape[0]))
+		src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+		dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+		M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+		
+
+		images[imgi]=cv2.warpPerspective(image2, np.linalg.inv(M), (image2.shape[1], image2.shape[0]))
+
+
 
 	print("alignment finished...")
 
-	return imgout
+	return images
 
 
 
@@ -86,70 +94,71 @@ def hist_match(source, template):
 	return interp_t_values[bin_idx].reshape(oldshape)
 
 
-def focus_stack(image1, image2):
+def focus_stack(baseimage, images):
 	print("start focus stacking")
 
 	if config.HIST_MATCHING:
-		img_yuv0 = cv2.cvtColor(image1, cv2.COLOR_BGR2YUV)
-		img_yuv1 = cv2.cvtColor(image2, cv2.COLOR_BGR2YUV)
+		img_yuv0 = cv2.cvtColor(baseimage, cv2.COLOR_BGR2YUV)
+
+		for imgi in range(len(images)):
+			image2=images[imgi]
+
+			img_yuv1 = cv2.cvtColor(image2, cv2.COLOR_BGR2YUV)
+
+			img_yuv1[:,:,0] = hist_match(img_yuv1[:,:,0],img_yuv0[:,:,0])
+			images[imgi] = cv2.cvtColor(img_yuv1, cv2.COLOR_YUV2BGR)
+
+	image1=baseimage
+
+	for image2 in images:
+		tmplap = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
+		tmplap = laplacian_trans(tmplap)
+		kernel = np.ones((5,5),np.float32)
+		tmplap = cv2.filter2D(tmplap,-1,kernel)
+		lap1 = np.absolute(tmplap)
 
 
-		img_yuv1[:,:,0] = hist_match(img_yuv1[:,:,0],img_yuv0[:,:,0])
-		image2 = cv2.cvtColor(img_yuv1, cv2.COLOR_YUV2BGR)
-
-	
-	tmplap = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
-	tmplap = laplacian_trans(tmplap)
-	kernel = np.ones((5,5),np.float32)
-	tmplap = cv2.filter2D(tmplap,-1,kernel)
-	lap1 = np.absolute(tmplap)
-
-
-	tmplap = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
-	tmplap = laplacian_trans(tmplap)
-	kernel = np.ones((5,5),np.float32)
-	tmplap = cv2.filter2D(tmplap,-1,kernel)
-	lap2 = np.absolute(tmplap)
+		tmplap = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
+		tmplap = laplacian_trans(tmplap)
+		kernel = np.ones((5,5),np.float32)
+		tmplap = cv2.filter2D(tmplap,-1,kernel)
+		lap2 = np.absolute(tmplap)
 
 
 
-	output = np.zeros(shape=image1.shape, dtype="float64")
-	output_count = np.zeros(shape=image1.shape, dtype="int")
-	
-
-	
-	
-
-	w=config.SLIDE_WINDOW_SIZE
-	step=config.SLIDE_WINDOW_STEP
-	
-	
-	x,y=0,0
-	while x<lap1.shape[0]:
-		while y<lap1.shape[1]:
-			dev1=deviation(x,y,w,lap1)
-			dev2=deviation(x,y,w,lap2)
-			if dev1>dev2:
-				output[x:x+w,y:y+w,:]+=image1[x:x+w,y:y+w,:]
-				output_count[x:x+w,y:y+w,:]+=1
-			elif dev2>dev1:
-				output[x:x+w,y:y+w,:]+=image2[x:x+w,y:y+w,:]
-				output_count[x:x+w,y:y+w,:]+=1
-			else:
-				tmpmean=image1[x:x+w,y:y+w,:]*0.5+image2[x:x+w,y:y+w,:]*0.5
-				output[x:x+w,y:y+w,:]+=tmpmean
-				output_count[x:x+w,y:y+w,:]+=1
-
-
-			y+=step
-		y=0
-		x+=step
+		output = np.zeros(shape=image1.shape, dtype="float64")
+		output_count = np.zeros(shape=image1.shape, dtype="int")
 		
-	output=output/output_count
 
-	print("finished")
+		w=config.SLIDE_WINDOW_SIZE
+		step=config.SLIDE_WINDOW_STEP
+		
+		
+		x,y=0,0
+		while x<lap1.shape[0]:
+			while y<lap1.shape[1]:
+				dev1=deviation(x,y,w,lap1)
+				dev2=deviation(x,y,w,lap2)
+				if dev1>dev2:
+					output[x:x+w,y:y+w,:]+=image1[x:x+w,y:y+w,:]
+					output_count[x:x+w,y:y+w,:]+=1
+				elif dev2>dev1:
+					output[x:x+w,y:y+w,:]+=image2[x:x+w,y:y+w,:]
+					output_count[x:x+w,y:y+w,:]+=1
+				else:
+					tmpmean=image1[x:x+w,y:y+w,:]*0.5+image2[x:x+w,y:y+w,:]*0.5
+					output[x:x+w,y:y+w,:]+=tmpmean
+					output_count[x:x+w,y:y+w,:]+=1
 
-	return output.astype(np.uint8)
+
+				y+=step
+			y=0
+			x+=step
+			
+		image1=(output/output_count).astype(np.uint8)
+
+
+	return image1.astype(np.uint8)
 
 
 
@@ -164,11 +173,10 @@ for name in glob.glob(config.IMAGE_PATH+"*.*"):
 	print(name)
 print("loading finished")
 
-image1=baseimage
-image2=align_image(baseimage,images[0])
+images=align_image(baseimage,images)
 
 
-result=focus_stack(image1,image2)
+result=focus_stack(baseimage,images)
 if config.DENOISE:
 	result=cv2.fastNlMeansDenoisingColored(result,None,h = 3,hColor = 3,templateWindowSize = 7,searchWindowSize = 21 )
 
